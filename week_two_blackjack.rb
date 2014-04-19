a030a0bdcc5929dfd77064b690bc6e8947090f6b
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-04-19 15:28:26 -0700

    up another draft. wish could add betting feature

--- week_two_blackjack.rb
+++ week_two_blackjack.rb
@@ -1,64 +1,49 @@
 class Game
   attr_accessor :deck, :dealer_cards, :player_cards
-  @@suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+  @@suits = ['Spade', 'Club', 'Heart', 'Diamond']
   @@ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
   def initialize
     @deck = @@suits.product(@@ranks).shuffle!
     @dealer_cards = Cards.new
     @player_cards = Cards.new
   end
-  def deal_first
-    2.times do
-      self.deal_player
-      self.deal_dealer
-    end
-  end
+
   def deal_player
     self.player_cards << self.deck.shift
-    puts "player now got #{self.player_cards}"
+    puts "player now got, #{self.player_cards.join(' ')}"
   end
   def deal_dealer
     self.dealer_cards << self.deck.shift
-    puts "dealer now got #{self.dealer_cards}"
+    puts "dealer now got, #{self.dealer_cards.join(' ')}"
   end
+
   def status
-    player_count = self.player_cards.count
-    dealer_count = self.dealer_cards.count
     puts "----------------------" 
-    puts "player".ljust(10) + "#{self.player_cards.size} cards".rjust(10)
-    puts "dealer".ljust(10) + "#{self.dealer_cards.size} cards".rjust(10)
-    puts "deck".ljust(10) + "#{self.deck.size} cards left".rjust(10)
+    puts "player".ljust(5) + "#{self.player_cards.count} points".rjust(10)
+    puts "dealer".ljust(5) + "#{self.dealer_cards.count} points".rjust(10)
+    # puts "#{self.deck.size} cards left in the deck"
     puts "----------------------"
-    puts "player_cards".ljust(10) + "#{player_count} points".rjust(10)
-    puts "dealer_cards".ljust(10) + "#{dealer_count} points".rjust(10)
-    puts "----------------------"
-    if (player_count < 21 && dealer_count < 21)
-      true
-    else
-      false
-    end
   end
+
   def end_comment
     player_count = self.player_cards.count
     dealer_count = self.dealer_cards.count
     if (player_count > 21 && dealer_count > 21) || (player_count == dealer_count)
       "Draw game"
     else
-      if player_count > 21
-        "You are BUSTED!"
-      elsif player_count == 21
-        "You are Black Jack!"
-      elsif dealer_count > 21
+      if dealer_count > 21
         "I am BUSTED!" 
+      elsif player_count > 21
+        "You are BUSTED!"
       elsif dealer_count == 21
         "I am Black Jack!" 
+      elsif player_count == 21
+        "You are Black Jack!"
       else
         if player_count > dealer_count
           "You win"
-        elsif player_count < dealer_count
-          "You lose"
         else
-          "what is this?"
+          "You lose"
         end
       end
     end
@@ -107,65 +92,98 @@ class Dealer
   end
 end
 
+# start a game
 game = Game.new
 
-# and player + dealer sitting
+# player + dealer sitting
 dealer = Dealer.new
 player = Player.new
 dealer.say "Hi, I'm dealer, my name is #{dealer.name}."
 player.name = (dealer.ask "What's your name?").capitalize
 dealer.say "Nice to meet you, #{player.name}. Let's start."
-game.deck = game.deck * (dealer.ask "Wait, how many deck?").to_i
+
+# prepare the shoe
+game.deck = game.deck * ((dealer.ask "Wait, how many deck?").to_i)
 game.deck.shuffle!
 # puts "cards in this game #{game.deck} and #{game.deck.size} in total"
 dealer.say "(that's #{game.deck.size} cards. shuffle... shuffle... )"
 sleep 2
 
-# continue
-continue = true
-while continue
+# play loop - while player in, until cards last
+in_play = true
+while in_play
+  if game.deck.size < 10
+    dealer.say "We need more cards to play. Bye-bye!"
+    break
+  end
 
-# first deal - two cards each (and count)
-game.deal_first
-
-not_finished = game.status
-player_stay = false
-dealer_stay = false
-while not_finished && !(dealer_stay && player_stay)
-  if !player_stay
-    player_decide = dealer.ask "1) hit or 2) stay?"
-    if player_decide == '1'
-      puts "player hit"
+  # first deal - two cards each (and count)
+  2.times do
       game.deal_player
-    elsif player_decide == '2'
-      puts "player stay"
-      player_stay = true
-    else
-      dealer.ask "Sorry I couldn't hear you, 1) hit or 2) stay?"
-      next
+      game.deal_dealer
+  end
+   
+  # status (1) after deal first two cards
+  game.status
+
+  # rally loop - while both under 21 and not both stay
+  # it's pass for either one who stays
+  player_stay = false
+  dealer_stay = false
+  while (game.player_cards.count < 21 && game.dealer_cards.count < 21) && !(dealer_stay && player_stay)
+  
+    unless player_stay 
+      # ask player whether hit or stay
+      # - deal a card to player if answer hit
+      # - move on to next person if answer stay
+      # - loop start over if answer not hit or stay ('1' or '2')
+      player_decide = dealer.ask "1) hit or 2) stay?"
+      if player_decide == '1'
+        puts "player hit"
+        game.deal_player
+      elsif player_decide == '2'
+        puts "player stay"
+        player_stay = true
+      else 
+        dealer.ask "Sorry I couldn't hear you, 1) hit or 2) stay?"
+        next 
+      end
     end
-  end
-  if game.dealer_cards.count < 17
-    puts "dealer hit"
-    game.deal_dealer
-  else
-    puts "dealer stay"
-    dealer_stay = true
-  end
-  not_finished = game.status
-end
+  
+    unless (dealer_stay || game.player_cards.count >= 21)
+      # by house rule, dealer always stay if over 17
+      # and always hit if equal or less than 17
+      # - no need to go if the player already over 21
+      if game.dealer_cards.count < 17
+        puts "dealer hit"
+        game.deal_dealer
+      else
+        puts "dealer stay"
+        dealer_stay = true
+      end
+    end
 
-puts "game end"
-dealer.say game.end_comment
+    # status (2) at the end of each turn
+    game.status 
 
-#
-c_answer = dealer.ask "continue? 1) yes 2) no"
-  if c_answer == '1'
+  end 
+  # end of rally loop
+
+  puts "end of play"
+  dealer.say game.end_comment # 
+
+  # start over this 'play' while loop 
+  # (+ clear cards on the table) if still in
+  in_answer = dealer.ask "continue? 1) yes"
+  if in_answer == '1'
     game.dealer_cards.clear
     game.player_cards.clear
-    continue = true
+    in_play = true
   else
-    continue = false
+    in_play = false
   end
-end
+
+end 
+# end of play loop
+
 exit
