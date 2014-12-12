6afc576c2ba257bcc82c9f85fc080b4d8c637b33
kangkyu <kangkyu1111@gmail.com>
2014-12-12 15:00:14 -0800

    require shoe_game file to player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,22 +1,26 @@
 require 'shoe_game'
 
 describe "Player" do
-
-  it "has a capitalized name" do
-    player1 = Player.new("larry")
-    player1.name.should == "Larry"
+  before do
+    @game = ShoeGame.new
+    @game.game_ready
   end
 
-  it "has an initial money" do  
-    player1 = Player.new("larry",100)
-    player1.money_current.should == 100
+  context "each player ready for the game" do
+    it "has a capitalized name" do
+      @game.player1 = Player.new('larry')
+      expect(@game.player1.name).to eq("Larry")
+    end
+
+    it "has an initial money" do  
+      player1 = Player.new("larry", 100)
+      expect(player1.money_current).to eq(100)
+    end
   end
 
   it "gets cards with face value" do
-    game = ShoeGame.new
-    game.game_ready
-    game.player1 << game.shoe.deal_one
-    expect(game.player1.cards.first.face_value).not_to be_nil
+    @game.player1 << @game.shoe.deal_one
+    expect(@game.player1.cards.first.face_value).not_to be_nil
   end
 
   context "soft aces counted as 1" do
@@ -24,28 +28,28 @@ describe "Player" do
       player1 = Player.new
       player1.cards = [Card.new('H','A'), Card.new('S','A')]
       player1.cards.each {|card| card.set_face_value }
-      player1.total_value.should == 1 + 11
+      expect(player1.total_value).to eq(1 + 11)
     end
 
     it "has total value not over 21 for three cards include an ace" do
       player1 = Player.new
       player1.cards = [Card.new('H','A'), Card.new('S','10'), Card.new('S','Q')]
       player1.cards.each {|card| card.set_face_value }
-      player1.total_value.should == 1 + 10 + 10
+      expect(player1.total_value).to eq(1 + 10 + 10)
     end
 
     it "has total value counted as the same when originally under 21" do
       player1 = Player.new
       player1.cards = [Card.new('H','A'), Card.new('S','2'), Card.new('S','3')]
       player1.cards.each {|card| card.set_face_value }
-      player1.total_value.should == 11 + 2 + 3
+      expect(player1.total_value).to eq(11 + 2 + 3)
     end
 
     it "has two hard aces in case still over 21 after one ace" do
       player1 = Player.new
       player1.cards = [Card.new('H','A'), Card.new('S','A'), Card.new('S','10'), Card.new('S','Q')]
       player1.cards.each {|card| card.set_face_value }
-      player1.total_value.should == 1 + 1 + 10 + 10
+      expect(player1.total_value).to eq(1 + 1 + 10 + 10)
     end
   end
 end
