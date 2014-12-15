f1e4bd09201d8889409d877030ca94a7de66745b
kangkyu <kangkyu1111@gmail.com>
2014-12-14 23:30:45 -0800

    merge dealer + player into person file

--- lib/person.rb
+++ lib/person.rb
@@ -48,14 +48,68 @@ end
 class Person
   include Comparable
   include Hand
+  include WithMultipleCards
 
   attr_accessor :cards, :name
   def initialize(name)
-    @cards = Array.new
+    @cards = []
     @name = name.capitalize
   end
 
   def hit?
     decide_hit
+  end
+end
+
+class Player < Person
+  START_AMOUNT = 100
+
+  attr_accessor :money_current
+  def initialize(name = 'player', money = START_AMOUNT)
+    super(name)
+    @money_current = money
+  end
+
+  def decide_hit
+    puts "hit or stay, #{name}?"
+    answer = gets.chomp
+    case answer.downcase
+    when 'hit', 'h', '1' then true
+    when 'stay', 's' ,'2' then false
+    else
+      puts "please type 'hit' or 'stay'"
+      decide_hit
+    end
+  end
+
+  def ask_bet_much
+    puts "how much do you bet? you have #{money_current}"
+    answer = gets.chomp
+    if answer.empty?
+      ask_bet_much
+    else
+      answer.to_i
+    end
+  end
+end
+
+class Dealer < Person
+  def initialize(name = 'dealer')
+    super
+  end
+
+  def decide_hit
+    sleep 1
+    total_value < 17
+  end
+
+  def flip_first_card
+    puts "#{name}'s first card was:"
+    puts cards[0]
+  end
+
+  def show_second_card
+    puts "#{name}'s second card showing:"
+    puts cards[1]
   end
 end
